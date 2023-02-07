dee59cc6c58c635ffa5263a1eb35effff159ec63
Liam1017 <elijahturany1415@gmail.com>
2023-02-07 17:34:47 -0600

    2023/02/07 17:34

--- ArrayListS23/CustomArrayList.h
+++ ArrayListS23/CustomArrayList.h
@@ -1,5 +1,42 @@
 #pragma once
-class CustomArrayList {
 
+template <typename Type>
+class CustomArrayList {
+	private:
+		Type data[10];
+		int size{ 0 };
+		int capacity{ 10 };
+	public:
+		CustomArrayList() {}
+		 
+		Type at(int index) {
+			if ((index < size) && (index >= 0)) {
+				return data[index];
+			}
+			throw "Out of bounds";
+		} 
+
+		void set(int index, Type value) {
+			if ((index < size) && (index >= 0)) {
+				data[index] = value;
+			}
+			else {
+				throw "Out of bounds";
+			}
+		}
+		
+		int getSize() {
+			return size;
+		}
+
+		void append(Type value) {
+			if (size < capacity) {
+				data[size] = value;
+				size++;
+			}
+			else {
+				throw "Array full";
+			}
+		}
 };
 
