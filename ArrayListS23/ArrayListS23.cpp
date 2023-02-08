52ef481326d47cc65c7bdafea05c9f55d2dffe08
Liam1017 <elijahturany1415@gmail.com>
2023-02-08 17:37:19 -0600

    2023/02/08 15:37

--- ArrayListS23/ArrayListS23.cpp
+++ ArrayListS23/ArrayListS23.cpp
@@ -1,6 +1,52 @@
+#include "CustomArrayList.h"
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello World!\n";
+void runTestOne() {
+	cout << "Running tests" << endl;
+	CustomArrayList<int> customArray1;
+	customArray1.append(1);
+	customArray1.append(6);
+	customArray1.append(15);
+	if (customArray1.getSize() != 3) {
+		cout << "Size is incorrect" << endl;
+	}
+
+	CustomArrayList<int> customArray2;
+	customArray2.append(1);
+	customArray2.append(6);
+	customArray2.append(15);
+	if ((customArray2.at(0) != 1) || (customArray2.at(1) != 6) || (customArray2.at(2) != 15)) {
+		cout << "Appended data is incorrect" << endl;
+	}
+
+	CustomArrayList<int> customArray3;
+	try {
+		for (int i = 0; i < 12; i++) {
+			customArray3.append(i);
+		}
+		cout << "Appending over capacity didn't throw exception" << endl;
+	}
+	catch(...) {}
+
+	CustomArrayList<int> customArray4;
+	try {
+		int van = customArray4.at(-1);
+		cout << "Accessing negative index didn't throw exception" << endl;
+	}
+	catch(...) {}
+
+	CustomArrayList<int> customArray5;
+	for (int i = 0; i < 12; i++) {
+		customArray5.append(i);
+	}
+	for (int i = 0; i < customArray5.getSize(); i++) {
+		cout << customArray5.at(i) << " ";
+	}
+
 }
+
+int main() {
+	runTestOne();
+}
+
--- ArrayListS23/CustomArrayList.h
+++ ArrayListS23/CustomArrayList.h
@@ -3,11 +3,36 @@
 template <typename Type>
 class CustomArrayList {
 	private:
-		Type data[10];
+		Type* data;
 		int size{ 0 };
 		int capacity{ 10 };
 	public:
-		CustomArrayList() {}
+		CustomArrayList() {
+			data = new Type[capacity];
+		}
+
+		~CustomArrayList() {
+			delete[] data;
+		}
+
+		CustomArrayList(const CustomArrayList<Type>& other) {
+			capacity = other.capacity;
+			size = other.size;
+			data = new Type[capacity];
+			for (int i = 0; i < size; i++) {
+				data[i] = other.data[i];
+			}
+		}
+
+		CustomArrayList operator=(const CustomArrayList<Type>& other) {
+			delete[] data;
+			capacity = other.capacity;
+			size = other.size;
+			data = new Type[capacity];
+			for (int i = 0; i < size; i++) {
+				data[i] = other.data[i];
+			}
+		}
 		 
 		Type at(int index) {
 			if ((index < size) && (index >= 0)) {
@@ -30,13 +55,30 @@ class CustomArrayList {
 		}
 
 		void append(Type value) {
+			/*if (size == capacity) {
+				this->expand();
+			}
+			data[size] = value;
+			size++;*/
+
 			if (size < capacity) {
 				data[size] = value;
 				size++;
 			}
 			else {
-				throw "Array full";
+				this->expand();
 			}
+		}
+
+		void expand() {
+			Type* tmp = new Type[capacity*2];
+			for (int i = 0; i < capacity; i++) {
+				tmp[i] = data[i];
+			}
+			delete[] data;
+			data = tmp;
+			tmp = nullptr;
+			capacity *= 2;
 		}
 };
 
