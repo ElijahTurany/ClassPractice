08cf74eb7f4c18ff2a606e1577f389b4e17f630d
Liam1017 <elijahturany1415@gmail.com>
2023-04-27 17:34:02 -0500

    2023/04/27 17:33

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -3,18 +3,15 @@
 using namespace std;
 
 int main() {
-    Graph g;
+    Graph g(10);
     g.addConnection(0, 1);
-    g.addConnection(1, 2);
-    g.addConnection(1, 3);
-    g.addConnection(1, 4);
-    g.addConnection(2, 3);
-    g.addConnection(2, 5);
-    g.addConnection(4, 6);
-    g.addConnection(4, 7);
+    g.addConnection(1, vector<int>{2, 3, 4});
+    g.addConnection(2, vector<int>{3, 5});
+    g.addConnection(4, vector<int>{6, 7});
     g.addConnection(5, 6);
     g.addConnection(6, 7);
     g.addConnection(8, 9);
     g.printMatrix();
-    cout << g.findPath(0, 1) << g.findPath(0, 8) << g.findPath(2, 4) << g.findPath(8, 9) << g.findPath(8, 3);
+    cout << g.findPath(0, 1) << g.findPath(0, 8) << g.findPath(2, 4) << g.findPath(8, 9) << g.findPath(8, 3) << endl;
+    //cout << g.findDistance(0, 1) << g.findDistance(0, 2);
 }
--- Graph/Graph.h
+++ Graph/Graph.h
@@ -5,13 +5,12 @@ using namespace std;
 
 class Graph {
 private:
-	bool matrix[10][10];
+	vector<vector<bool>> matrix;
 public:
-	Graph() {
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				matrix[i][j] = false;
-			}
+	Graph(int size) {
+		vector<bool> vec(size, false);
+		for (int i = 0; i < size; i++) {
+			matrix.push_back(vec);
 		}
 	}
 
@@ -20,11 +19,39 @@ public:
 		matrix[b][a] = true;
 	}
 
+	void addConnection(int a, vector<int> b) {
+		for (int i = 0; i < b.size(); i++) {
+			matrix[a][b[i]] = true;
+			matrix[b[i]][a] = true;
+		}
+	}
+
 	void printMatrix() {
-		cout << "   0 1 2 3 4 5 6 7 8 9" << endl << "  ---------------------" << endl;
-		for (int i = 0; i < 10; i++) {
+		cout << "    ";
+		for (int i = 0; i < matrix.size(); i++) {
+			if (i / 10 == 0) {
+				cout << "  ";
+			}
+			else {
+				cout << i / 10 << " ";
+			}
+		}
+		cout << endl << "    ";
+		for (int i = 0; i < matrix.size(); i++) {
+			cout << i % 10 << " ";
+		}
+		cout << endl << "   -";
+		for (int i = 0; i < matrix.size(); i++) {
+			cout << "--";
+		}
+		cout << endl;
+
+		for (int i = 0; i < matrix.size(); i++) {
+			if (i < 10) {
+				cout << " ";
+			}
 			cout << i << "| ";
-			for (int j = 0; j < 10; j++) {
+			for (int j = 0; j < matrix.size(); j++) {
 				if (matrix[i][j]) {
 					cout << "X ";
 				}
@@ -34,10 +61,20 @@ public:
 			}
 			cout << "|" << endl;
 		}
-		cout << "  ---------------------" << endl;
+		cout << "   -";
+		for (int i = 0; i < matrix.size(); i++) {
+			cout << "--";
+		}
+		cout << endl;
 	}
 
 	bool findPath(int a, int b) {
+		if (a >= matrix.size() || b >= matrix.size() || a < 0 || b < 0) {
+			return false;
+		}
+		if (matrix[a][b]) {
+			return true;
+		}
 		bool visited[10];
 		vector<int> queue;
 		for (int i = 0; i < 10; i++) {
@@ -50,23 +87,20 @@ public:
 			if (matrix[a][i]) {
 				queue.push_back(i);
 			}
-			if (matrix[a][b]) {
-				return true;
-			}
 		}
 		return recursiveHelper(queue[0], b, queue, visited);
 	}
 
 	bool recursiveHelper(int a, int b, vector<int> queue, bool visited[10]) {
+		if (matrix[a][b]) {
+			return true;
+		}
 		queue.erase(queue.begin());
 		visited[a] = true;
 
 		for (int i = 0; i < 10; i++) {
 			if (matrix[a][i] && !visited[i]) {
 				queue.push_back(i);
-			}
-			if (matrix[a][b]) {
-				return true;
 			}
 		}
 		if (queue.size() == 0) {
@@ -76,4 +110,50 @@ public:
 			return recursiveHelper(queue[0], b, queue, visited);
 		}
 	}
+
+	/*int findDistance(int a, int b) {
+		if (a >= matrix.size() || b >= matrix.size() || a < 0 || b < 0) {
+			return -1;
+		}
+		if (matrix[a][b]) {
+			return 1;
+		}
+		if (a==b && matrix[a][b]) {
+			return 0;
+		}
+		bool visited[10];
+		vector<int> queue;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				visited[i] = false;
+			}
+		}
+		visited[a] = true;
+		for (int i = 0; i < 10; i++) {
+			if (matrix[a][i]) {
+				queue.push_back(i);
+			}
+		}
+		return recursiveHelper(queue[0], b, queue, visited);
+	}
+
+	int distanceHelper(int a, int b, vector<int> queue, bool visited[10]) {
+		if (matrix[a][b]) {
+			return 1;
+		}
+		queue.erase(queue.begin());
+		visited[a] = true;
+
+		for (int i = 0; i < 10; i++) {
+			if (matrix[a][i] && !visited[i]) {
+				queue.push_back(i);
+			}
+		}
+		if (queue.size() == 0) {
+			return -1;
+		}
+		else {
+			return 1 + distanceHelper(queue[0], b, queue, visited);
+		}
+	}*/
 };
